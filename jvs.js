19010a9e7e72f4fc75c9e38dca1dc7e8c5989123
dokhoa101 <135789553+dokhoa101@users.noreply.github.com>
2023-07-16 14:23:41 +0700

    Delete jvs.js

--- jvs.js
+++
@@ -1,9 +0,0 @@
-let number1 = parseFloat(prompt("Nhập số thứ nhất: "));
-let number2 = parseFloat(prompt("Nhập số thứ hai: "));
-
-// Tính tổng hai số
-let sum = number1 + number2;
-
-// In kết quả ra console
-console.log(`Tổng của ${number1} và ${number2} là ${sum}.`);
-alert(sum);
